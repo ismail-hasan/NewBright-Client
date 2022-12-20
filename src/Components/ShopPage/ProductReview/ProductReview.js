fcdab9e185a1ab61fb4702d47633fe6dea387051
unknown <fnismailhasan@gmail.com>
2022-12-20 12:45:14 +0600

    review added

--- src/Components/ShopPage/ProductReview/ProductReview.js
+++ src/Components/ShopPage/ProductReview/ProductReview.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { toast } from 'react-hot-toast';
 import ReactStars from 'react-rating-stars-component';
+import ReviewInfo from './ReviewInfo';
 
 
 
@@ -40,39 +41,44 @@ const ProductReview = () => {
 
 
     return (
-        <div className='px-20 mb-20'>
-            <h1 className='text-3xl mb-5 font-semibold'>Write A Review</h1>
-            <form onSubmit={handleForm} action="">
-                <p>
-                    <label htmlFor="">Name: </label> <br />
-                    <input required name='reivewName' type="text" placeholder="Type here" className="input input-bordered w-full" />
-                </p>
-                <p className='mt-7'>
-                    <label htmlFor="">Message: </label> <br />
-                    <textarea required name='reivewDec' className="textarea textarea-ghost input-bordered w-full h-36" placeholder="Bio"></textarea>
-                </p>
-                <div className='flex mt-5'>
-                    <p className='text-[24px] mr-4'> Rating :</p>
-                    <div>
-                        <ReactStars
-                            count={5}
-                            onChange={ratingChanged}
-                            size={31}
-                            activeColor="#ffd700"
-                            isHalf={true}
-                            value={5}
+        <>
+            <div className='px-20 mb-20'>
+                <div className=''>
+                    <ReviewInfo></ReviewInfo>
+                </div>
+                <h1 className='text-3xl mb-5 font-semibold'>Write A Review</h1>
+                <form onSubmit={handleForm} action="">
+                    <p>
+                        <label htmlFor="">Name: </label> <br />
+                        <input required name='reivewName' type="text" placeholder="Type here" className="input input-bordered w-full" />
+                    </p>
+                    <p className='mt-7'>
+                        <label htmlFor="">Message: </label> <br />
+                        <textarea required name='reivewDec' className="textarea textarea-ghost input-bordered w-full h-36" placeholder="Bio"></textarea>
+                    </p>
+                    <div className='flex mt-5'>
+                        <p className='text-[24px] mr-4'> Rating :</p>
+                        <div>
+                            <ReactStars
+                                count={5}
+                                onChange={ratingChanged}
+                                size={31}
+                                activeColor="#ffd700"
+                                isHalf={true}
+                                value={5}
 
 
-                        />
+                            />
+                        </div>
                     </div>
-                </div>
-                <div>
+                    <div>
 
-                </div>
+                    </div>
 
-                <button className='bg-black py-2 mt-5 px-8 rounded-[30px] text-white'>Continue</button>
-            </form>
-        </div>
+                    <button className='bg-black py-2 mt-5 px-8 rounded-[30px] text-white'>Continue</button>
+                </form>
+            </div>
+        </>
     );
 };
 
---
+++ src/Components/ShopPage/ProductReview/ReviewInfo.js
@@ -0,0 +1,78 @@
+import React, { useEffect, useState } from 'react';
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination, Navigation } from "swiper";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import "./Review.css";
+import { useQuery } from '@tanstack/react-query';
+
+
+
+const ReviewInfo = () => {
+    // const [reviewInfo, setReviewInfo] = useState([])
+
+
+
+    const { data: reviewInfo = [], refetch, isLoading } = useQuery({
+        queryKey: ["review"],
+        queryFn: async () => {
+            const res = await fetch("http://localhost:5000/review")
+            const data = await res.json()
+            return data
+        }
+    })
+    if (isLoading) {
+        return <h1>dsfsdf</h1>
+    }
+
+    // useEffect(() => {
+    //     fetch("http://localhost:5000/review")
+    //         .then(res => res.json())
+    //         .then(data => {
+    //             console.log(data)
+    //             setReviewInfo(data)
+    //                 ()
+    //         })
+    // }, [refetch])
+    refetch()
+
+    return (
+        <div className='my-20'>
+            <Swiper
+                slidesPerView={1}
+                spaceBetween={10}
+                loop={true}
+                style={{ height: "50vh", width: "80vw", backgroundColor: '#eee' }}
+                pagination={{
+                    clickable: true,
+                }}
+                grabCursor
+                navigation={true}
+                modules={[Pagination, Navigation]}
+                className="mySwiper"
+            >
+
+                <div className=''>
+                    {
+                        reviewInfo.map((item, idx) => {
+                            return (
+                                <div key={idx}>
+                                    <SwiperSlide>
+                                        <div >
+                                            <p>{item.reviweName}</p>
+                                            <p>{item.reviweDec}</p>
+                                        </div>
+                                    </SwiperSlide>
+                                </div>
+                            )
+                        })
+                    }
+                </div>
+
+            </Swiper>
+        </div >
+    );
+};
+
+export default ReviewInfo;
